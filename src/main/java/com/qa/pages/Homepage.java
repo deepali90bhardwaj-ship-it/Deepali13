751fb6786eaed533ff729d12fe4dfd3d973ae4e5
abcd <dee@gmail.com>
2025-09-05 11:56:44 +0530

    my new project

---
+++ src/main/java/com/qa/pages/Homepage.java
@@ -0,0 +1,10 @@
+package com.qa.pages;
+
+public class Homepage {
+	
+	public void home()
+	{
+		System.out.println("hi home page");
+	}
+
+}
---
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -0,0 +1,8 @@
+package com.qa.pages;
+
+public class LoginPage {
+
+	public void login() {
+		System.out.println("Hi login page");
+	}
+}
