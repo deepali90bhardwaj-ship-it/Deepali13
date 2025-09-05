9b7a3471665751c2982f1f91e60fee46e19f7855
Deepali <deepali90bhardwaj@gmail.com>
2025-09-05 13:28:16 +0530

    Update LoginPage.java

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -4,6 +4,6 @@ public class LoginPage {
 
 	public void login() {
 		System.out.println("Hi login page");
-		System.out.println("Hi login page .123");
+		System.out.println("Hi login page .123avgfhvhajfvhaj");
 	}
 }
