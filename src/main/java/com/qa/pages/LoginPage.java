868d9aab07ebb91cbc124b52d0515ab875293709
Deepali <deepali90bhardwaj@gmail.com>
2025-09-07 06:56:16 +0530

    Update LoginPage.java

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -7,11 +7,6 @@ public class LoginPage {
 System.out.println("Hi login page .123avgfhvhajfvhgvgvhgvhjvjhvsdfsdfaj");		
 System.out.println("Hi login page .123avgfhvhajfvhgvgvhgvhjvjhvaj");
 System.out.println("Hi login page .123avgfhvhajfvhgvgvhgvhjvjhvaj");
-<<<<<<< HEAD
-System.out.println("Hi login page .123avgfhvhajfvhgvgvhgvhjvjhvaj");
-=======
-		System.out.println("Hi login page .123avgfhvhajfvhgvgvhgvhjvjhvaj");
-		System.out.println("Hi login page .123avgfhvhajfvhgvgvhgvhjvjhvaj");
->>>>>>> c46bec8e1a9c6677a98dff4582a68f4a8f3c4451
+
 	}
 }
